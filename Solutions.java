16e1bafcd8952f29ab28408e98fe9738c6927d2c
laurairwin <lirwin24@cgps.org>
2024-02-06 12:30:09 -0500

    tUESDAYS work

---
+++ Solutions.java
@@ -0,0 +1,9 @@
+public class Solutions {
+
+    public static int [][] scale (int[][] input, int scalingFactor){
+
+        //iterate through the 2D array input and for each value, im
+        // going to multiply it by the scaling factor and place that value
+        // in the output array in the same spot
+    }
+}
