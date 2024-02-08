65cff103b04b54b18cb1a5482a0245aed1bbb0e9
laurairwin <lirwin24@cgps.org>
2024-02-08 14:07:37 -0500

    tUESDAYS work

--- Solutions.java
+++ Solutions.java
@@ -1,6 +1,6 @@
 public class Solutions {
 
-    public static int [][] scale (int[][] input, int scalingFactor){
+
 
         //iterate through the 2D array input and for each value, im
         // going to multiply it by the scaling factor and place that value
--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
-
+        public static int [][] scale (int[][] input, int scalingFactor){
+            int [][] inputArr1 = {{11,12,13},{11,12,13}};
+            int [][] inputArr2 = {{11,12},{11,12}};
+            Solutions sol = new Solutions();
+            sol.print(sol.scale(inputArr1,))
     }
 }
