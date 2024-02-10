60cdbee87649f877cc94d9c784e4a09af80a3da9
laurairwin <lirwin24@cgps.org>
2024-02-09 23:18:50 -0500

    tUESDAYS work

--- Solutions.java
+++ Solutions.java
@@ -1,9 +1,43 @@
 public class Solutions {
+//scale the matrix by scale factor using a for loop
+    public static int[][]scale(int[][]matrix, int scaleFactor){
+        for(int i = 0; i< matrix.length; i++){
+            for(int j =0; i<matrix[i].length; i++){
+                matrix[i][j]*=scaleFactor;
+            }
+        }
+return matrix;
 
+    } //multiply any matrices that are the same length
+    public static int[][] timesMatrices(int[][] matrix1, int[][]matrix2){
+        if(matrix1[0].length!= matrix2.length){
+            System.out.println("Matrices cannot be multiplied together");
+            return null;
+        }
+        int[][] result = new int[matrix1.length][matrix2[0].length];
+        for(int i = 0; i<matrix1.length; i++){
+            for(int j = 0; j< matrix2[0].length; j++){
+                for(int k = 0; k< matrix1[0].length;k++){
+                    result[i][j] +=matrix1[i][k]* matrix2[k][j];
+                }
 
+            }
+        }
+        return result;
+    }
 
-        //iterate through the 2D array input and for each value, im
-        // going to multiply it by the scaling factor and place that value
-        // in the output array in the same spot
+    public static int findDeterminant2x2(int[][] matrix){
+        if(matrix.length != 2 || matrix[0].length !=2){
+            System.out.println("Wrong for 2x2");
+            return 0;
+        }
+    public static int findDeterminant3x3(int[][] matrix){
+        if(matrix.length != 3 || matrix[0].length !=3){
+            System.out.println("Wrong for 3x3");
+            return 0;
+        }
+        return matrix[0][0] * (matrix [1][1] * matrix [2][2]-matrix[1][2]*matrix[2][1]) -
+                matrix[0][1] * (matrix [1][0] * matrix [2][2]-matrix[1][2]*matrix[2][0]) +
+                matrix[0][2] * (matrix [1][0] * matrix [2][1]-matrix[1][1]*matrix[2][0]);
     }
 }
--- main.java
+++ main.java
@@ -1,9 +1,56 @@
 public class main {
     public static void main(String[] args) {
-        public static int [][] scale (int[][] input, int scalingFactor){
-            int [][] inputArr1 = {{11,12,13},{11,12,13}};
-            int [][] inputArr2 = {{11,12},{11,12}};
-            Solutions sol = new Solutions();
-            sol.print(sol.scale(inputArr1,))
-    }
+
+            int [][] matrixLaura2x2 = {{4,3},{2,1}};
+        int [][] matrixSophie3x3 = {{8,9,7},{6,5,4},{3,2,1}};
+        int [][] matrixForScale = {{2,3},{4,5}};
+        int scaleFactor = 2;
+
+        int [][] scaledMatrix = Solutions.scale(matrixForScale, scaleFactor);
+        System.out.println("The Scaled Matrix");
+        printMatrix(scaledMatrix);
+
+        int [][] timesMatrix = Solutions.timesMatrices(matrixForScale, matrixLaura2x2);
+        System.out.println("the multiplied matrices");
+        printMatrix(timesMatrix);
+
+        int determinant2x2 = Solutions.findDeterminant2x2(matrixLaura2x2);
+        System.out.println("Determinant of Laura's 2x2 is"+ determinant2x2);
+
+        int determinant3x3 = Solutions.findDeterminant3x3(matrixSophie3x3);
+        System.out.println("Determinant of Sophie's 3x3 is"+ determinant3x3);
+   }
+
+   private static void printMatrix(int[][]matrix){
+        for(int [] row : matrix){
+            for(int element : row){
+                System.out.println(element + "");
+            }
+            int [][] scaledMatrix = Solutions.scale(matrixForScale, scaleFactor);
+            System.out.println("scaled matrix is");
+            printMatrix(scaledMatrix);
+
+            int[][] timesMatrix = Solutions.timesMatrices(matrixForScale,matrixLaura2x2);
+            System.out.println("the multiplied Matrix are");
+            printMatrix(timesMatrix);
+
+            int determinant2x2 = Solutions.findDeterminant2x2(matrixLaura2x2);
+            System.out.println("Determinant of Lauras 2x2 Matrix is"+determinant2x2);
+
+            int determinant2x3 = Solutions.findDeterminant3x3(matrixSophie3x3);
+            System.out.println("Determinant of Sophies 3x3 Matrix is" + determinant3x3);
+
+        }
+
+        private static void printMatrix(int[][] matrix){
+            for(int[] row : matrix){
+                for(int element : row){
+                    System.out.print(element + "");
+
+                }
+                System.out.println();
+            }
+            System.out.println();
+       }
+   }
 }
